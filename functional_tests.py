17bbfaa8fc309558dad5065797ed055e60e5e17e
buginventor <sm3rda@gmail.com>
2019-04-12 23:05:55 +0200

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -19,7 +19,7 @@ class NewvisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertin('To-Do', header_text)
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -32,7 +32,8 @@ class NewvisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock featkers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), 
+                'New to-do item did not appear in table')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
